651d95c27c4a6720e0009cbaecab3d9e1c1eaf91
marinkapuki <marina.tarasevich.2015@gmail.com>
2024-11-29 16:39:05 +0700

    Итерация 2: Прописаны основные методы, добавлены вспомогательные в виде заглушек.

--- anagram.py
+++ anagram.py
@@ -3,7 +3,18 @@ class AnagramFinder:
         self.text = text
 
     def find_anagrams(self):
-        pass  # Заглушка для метода нахождения анаграмм
+        words = self.text.split()
+        anagrams = {}
+        for word in words:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+        return {key: value for key, value in anagrams.items() if len(value) > 1}
 
-    def display_results(self):
-        return "образец"  # Заглушка для метода отображения результатов
+    def display_results(self, results):
+        if results:
+            return f"Найденные анаграммы: {results}"
+        else:
+            return "Анаграммы не найдены."
