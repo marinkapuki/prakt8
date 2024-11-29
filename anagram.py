c5d7a2ff5218beada1703d7edee8a53986d0ea3d
marinkapuki <marina.tarasevich.2015@gmail.com>
2024-11-29 16:37:10 +0700

    Итерация 1: Объявлены все классы. Все основные методы в виде заглушек.

---
+++ anagram.py
@@ -0,0 +1,9 @@
+class AnagramFinder:
+    def __init__(self, text):
+        self.text = text
+
+    def find_anagrams(self):
+        pass  # Заглушка для метода нахождения анаграмм
+
+    def display_results(self):
+        return "образец"  # Заглушка для метода отображения результатов
